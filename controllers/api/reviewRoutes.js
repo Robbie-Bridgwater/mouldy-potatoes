25b0053883d63e153343c837d61d18f15f2c151d
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-04-03 12:02:15 +0100

    did the user and review routes

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { Review, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async(req, res) => {
+    try {
+        const newReview = await Review.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newReview);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/:id', withAuth, async(req, res) => {
+    try {
+        const newComment = await Comment.create({
+            comment_text: req.body.comment_text,
+            review_id: req.body.review_id,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async(req, res) => {
+    try {
+        const reviewData = await Review.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!reviewData) {
+            res.status(404).json({ message: 'No review found with this id!' });
+            return;
+        }
+
+        res.status(200).json(reviewData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
