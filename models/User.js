fef1f9a4d39878968f271cc87db8a23f26d503fc
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-04-03 10:21:11 +0100

    Model for user added

--- models/User.js
+++ models/User.js
@@ -7,3 +7,32 @@ class User extends Model {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
+
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      },
+    },
