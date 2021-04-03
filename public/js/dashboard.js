0471f315b988ab6517d7835386d2a49a83885966
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-04-03 11:58:55 +0100

    dashboard.js completed

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -20,3 +20,27 @@ const newFormHandler = async (event) => {
       }
     }
   };
+
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/reviews/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete review');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-review-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.review-list')
+    .addEventListener('click', delButtonHandler);
