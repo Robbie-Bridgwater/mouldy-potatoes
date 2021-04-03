3274496f794a9be269a9cc2210e04025ebbcff3c
Karen <karenyaa@live.co.uk>
2021-04-03 11:47:34 +0100

    added route for login in home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,4 +62,24 @@ router.get("/review/:id", async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  // Route to signup page
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('signup');
+});
+
 module.exports = router;
