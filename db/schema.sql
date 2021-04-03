2b9090da8b2aa0510469d323df3105af5cc1cd0e
Robbie Bridgwater <71599797+Robbie-Bridgwater@users.noreply.github.com>
2021-04-03 12:51:31 +0100

    Revert "Login route - completed the login routes. "

--- db/schema.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS review_db;
-CREATE DATABASE review_db;
--- utils/helpers.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  format_date: (date) => {
-    // Format date as DD/MM/YYYY
-    return date.toLocaleDateString();
-  },
-};
