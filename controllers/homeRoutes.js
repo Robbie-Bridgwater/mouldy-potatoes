a6e8da433651ac9042aa94ba0bd4d0d848767f4a
Karen <karenyaa@live.co.uk>
2021-04-03 11:51:35 +0100

    added get route for dashboard in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,6 +62,26 @@ router.get("/review/:id", async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Review }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
