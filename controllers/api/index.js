def447b12fee5e1af9a4f1e9388b7938ccda51a9
Ben <pauleybj@gmail.com>
2021-04-03 13:13:36 +0100

    uncommented code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-// const userRoutes = require("./userRoutes");
-// const reviewRoutes = require("./reviewRoutes");
+const userRoutes = require("./userRoutes");
+const reviewRoutes = require("./reviewRoutes");
 // const commentRoutes = require("./commentRoutes");
 
-// router.use("/users", userRoutes);
-// router.use("/reviews", reviewRoutes);
+router.use("/users", userRoutes);
+router.use("/reviews", reviewRoutes);
 // router.use("/comments", commentRoutes);
 
 module.exports = router;
