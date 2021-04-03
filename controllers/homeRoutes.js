51b5ed346ae5b6dd6321bb59e97cdde5e029f46f
Karen <karenyaa@live.co.uk>
2021-04-03 11:54:57 +0100

    added put route for editing reviews on dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -117,4 +117,28 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
+router.put('/dashboard/edit/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update({
+      name: req.body.name,
+      description: req.body.description
+    },
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No blog found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
